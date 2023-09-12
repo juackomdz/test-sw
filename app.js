b23c013727477b5c48e357a23853294ec450eb08
Joaquin Mendez <joaquin.enmt@gmail.com>
2023-09-12 14:47:59 -0300

    Test Offline 2

--- app.js
+++ app.js
@@ -1,37 +1,62 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
+import { getFirestore, collection, doc, setDoc, addDoc } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCu4h1BDCC48XsP_y8vu0ttFPWd-BcltMY",
-  authDomain: "user-8c0e9.firebaseapp.com",
-  projectId: "user-8c0e9",
-  storageBucket: "user-8c0e9.appspot.com",
-  messagingSenderId: "892970892840",
-  appId: "1:892970892840:web:40d41e0b96c159490ebf1d"
+
+  apiKey: "AIzaSyDVCBhNH-Ht-qyEZvbsOqq378__YSxMqco",
+
+  authDomain: "testing-offline-e2529.firebaseapp.com",
+
+  projectId: "testing-offline-e2529",
+
+  storageBucket: "testing-offline-e2529.appspot.com",
+
+  messagingSenderId: "1070849200316",
+
+  appId: "1:1070849200316:web:8fe127db3ceea6e071c0b9"
+
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-document.getElementById('btn').addEventListener("click", function() {
-//function guarda(){
-    var email = document.getElementById('correo').value;
-    var password = document.getElementById('pass').value;
-  if(password.length>=6){
-    createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
-  }else{
-    alert ("la contraseña debe tener al menos 6 caracteres");
-  } 
 
-  });
+//function guardar(){
+  //guardarUser;
+  //var nombre = document.getElementById('nombre').value;
+  //var apellido = document.getElementById('apellido').value;
+
+  //db.collection("usuarios").add({
+    //nombre: nombre,
+    //apellido: apellido
+  //})
+  //.then((docRef) => {
+    //console.log("Document written with ID: ", docRef.id);
+  //})
+  //.catch((error) => {
+    //console.error("Error adding document: ", error);
+  //});
+//}
+
+document.getElementById('btn').addEventListener("click", async function () {
+  //guardarUser;
+  var nombre = document.getElementById("nombre").value;
+  var apellido = document.getElementById("apellido").value;
+  try {
+    const docRef = await addDoc(collection(db, "usuarios"), {
+      nombre: nombre,
+      apellido: apellido
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+});
+
+//const guardarUser = async() =>{
+  
+//}
