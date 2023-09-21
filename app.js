078ca26f76aa8d49a70c2e61d017603ba27fbba6
Joaquin Mendez <joaquin.enmt@gmail.com>
2023-09-21 11:12:22 -0300

    Test Offline 7

--- app.js
+++ app.js
@@ -19,31 +19,87 @@ const firebaseConfig = {
 };
 
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
 document.getElementById('cons').addEventListener("click", function () {
-  
+
   //alert('Los datos almacenados son: Nombre: ' + localStorage.getItem('nombre'));
-  window.location.href='offline.html'
+  window.location.href = 'offline.html'
 })
 
-document.getElementById('btn').addEventListener("click", async function () {
+window.addEventListener('offline', function () {
+
+  function store() {
+
+    var comn1 = document.getElementById('comentario1');
+    var comn2 = document.getElementById('comentario2');
+    var check_a = document.getElementById('chka');
+    var check_b = document.getElementById('chkb');
+    if (typeof Storage !== 'undefined') {
+      if (comn1.value.length >= 1) {
+        localStorage.setItem('coment1', comn1.value);
+      }
+      if (comn2.value.length >= 1) {
+        localStorage.setItem('coment2', comn2.value);
+      }
+
+
+      if (check_a.checked) {
+        localStorage.setItem('cha', check_a.value);
+      }
+
+      if (check_b.checked) {
+        localStorage.setItem('chb', check_b.value);
+      }
+
+      return true;
+    }
+    return false;
+  }
+
+  document.getElementById('btn').addEventListener('click', function () {
+    //localStorage.clear()
+    store()
+  })
+  
+});
+
+
+
+
+document.getElementById('btn').addEventListener('click', async function () {
   //guardarUser;
-  var nombre = document.getElementById("nombre").value;
-  var coment = document.getElementById("comentario").value;
+  var comn1 = document.getElementById('comentario1').value;
+  var comn2 = document.getElementById('comentario2').value;
+  var check_a = document.getElementById('chka');
+  var check_b = document.getElementById('chkb');
+  if(check_a.checked){
+    var check1=check_a.value
+  }
+  else{
+    var check1=null
+  }
+  if(check_b.checked){
+    var check2=check_b.value
+  }
+  else{
+    var check2=null
+  }
+  //var nombre = document.getElementById("nombre").value;
+  //var coment = document.getElementById("comentario").value;
   try {
-    const docRef = await addDoc(collection(db, "persona"), {
-      nombre: nombre,
-      comentario: coment
+    const docRef = await addDoc(collection(db, "datos"), {
+      comentario_1: comn1,
+      comentario_2: comn2,
+      check1: check1,
+      check2: check2
     });
     console.log("Document written with ID: ", docRef.id);
   } catch (e) {
     console.error("Error adding document: ", e);
   }
 });
-
-
-
